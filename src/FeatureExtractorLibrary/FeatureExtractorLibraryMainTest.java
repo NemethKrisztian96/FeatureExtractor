181e71dc49d43375e8e41fb14461f7974e5a0a22
NemethKrisztian96 <n.krisztian96@gmail.com>
2019-02-06 19:48:43 +0200

    Added proper usage example

--- src/FeatureExtractorLibrary/FeatureExtractorLibraryMainTest.java
+++ src/FeatureExtractorLibrary/FeatureExtractorLibraryMainTest.java
@@ -12,6 +12,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -36,42 +37,45 @@ public class FeatureExtractorLibraryMainTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         //THESE ARE ALL JUST TEST
         //THEY CAN GIVE YOU INSIGHT ABOUT THE LIBRARY'S USAGE
-        
-        Settings.usingFrames(128);
-        String IOFolder = "../FeatureExtractorLibrary_IO_files/";
-        
+        //String IOFolder = "../FeatureExtractorLibrary_IO_files/";
+        String IOFolder = "data/";
+        String inputFileName = "data_ttJMxBAjuHNVLCKhaXNvBTFDbIc2_20181122_161810.csv";
+        String outputFileName = "features_ttJMxBAjuHNVLCKhaXNvBTFDbIc2_20181122_161810";
+        //settings regarding the input and output files
+        Settings.setInputHasHeader(true); //input has a header that has to be skipped
+        Settings.setOutputHasHeader(true); //output will have a header
+        Settings.setOutputFileType(Settings.FileType.ARFF); //output will be an .arff file
+        Settings.setDefaultUserId("dummy");
+
+        //if we would like to use walking cycles based feature extraction
+        //Settings.usingCycles(); 
+        //Settings.setNumStepsIgnored(1); //ignoring first and last step
+        //if we would like to use walking cycles based feature extraction
+        Settings.usingFrames(128); //using frames made of 128 datapoints
+        Settings.setNumFramesIgnored(2); //ignoring first and last 2 frames (256 datapoints in this scenario)
+
+        //now based on the previous settings we are extracting features from the input file
         try {
-            Settings.setOutputHasHeader(true);
-            Settings.setOutputFileType(Settings.FileType.ARFF);
-            //Settings.usingCycles();
-            //Settings.setNumStepsIgnored(2);
-            //Settings.setNumFramesIgnored(2);
-            FeatureExtractor.extractFeaturesFromCsvFileToFile(IOFolder + "rawdata_WsY044SgeaeZtDrQKVpRyWpo7hx1_20181212_133019.csv", IOFolder + "features_WsY044SgeaeZtDrQKVpRyWpo7hx1_20181212_133019");
+            //extracting into a file
+            FeatureExtractor.extractFeaturesFromCsvFileToFile(IOFolder + inputFileName, outputFileName);
+
+            //extracting into a list
+            List<Feature> featureList = FeatureExtractor.extractFeaturesFromCsvFileToArrayListOfFeatures(IOFolder + inputFileName);
+            //System.out.println(featureList);
         } catch (FeatureExtractorException ex) {
             Logger.getLogger(FeatureExtractorLibraryMainTest.class.getName()).log(Level.SEVERE, null, ex);
         }
-            
-        /*try {
-        //Settings.setOutputHasHeader(true);
-        Settings.setDefaultUserId("dasdsaa");
-        Settings.setOutputFileType(Settings.FileType.CSV);
-        FeatureExtractor.extractFeaturesFromCsvFileToFile("rawdata_in11.csv", "features_LnntbFQGpBeHx3RwMu42e2yOks32_20181130_164700");
-        } catch (FeatureExtractorException ex) {
-            Logger.getLogger(FeatureExtractorLibraryMainTest.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
-        //System.out.println((new Util()).arrayListOfFeaturesToInstances(FeatureExtractor.extractFeaturesFromCsvFileToArrayListOfFeatures("rawdata_LnntbFQGpBeHx3RwMu42e2yOks32_20181130_164700.csv")));
-    
-        System.out.println("done");
-        System.out.println(Settings.getAllSettings());
-        /*
+
+        //now we are extracting features from an ArrayList<Feature>
+        //first we read the data
         Scanner scanner = null;
         try {
-            scanner = new Scanner(new File("in1.csv"));
+            scanner = new Scanner(new File(IOFolder + inputFileName));
         } catch (FileNotFoundException ex) {
-            System.out.println("Unable to open file " + "in1.csv");
+            System.out.println("Unable to open file " + IOFolder + inputFileName);
             System.exit(1);
         }
 
@@ -92,25 +96,23 @@ public class FeatureExtractorLibraryMainTest {
                 System.out.println("Corrupted input file error");
                 return;
             }
-            dataset.add(new Accelerometer(Long.parseLong(items[0]),
-                    Double.parseDouble(items[1]),
-                    Double.parseDouble(items[2]),
-                    Double.parseDouble(items[3]),
-                    Integer.parseInt(items[4])));
+            dataset.add(new Accelerometer(Long.parseLong(items[0]), //timesptamp
+                    Double.parseDouble(items[1]), //X
+                    Double.parseDouble(items[2]), //Y
+                    Double.parseDouble(items[3]), //Z
+                    Integer.parseInt(items[4])));                    //stepNumber
         }
-        Settings.usingCycles();
+
+        //exract features from the dataset
         try {
-            FeatureExtractor.extractFeaturesFromArrayListToCsvFile(dataset, "out4.csv", "1");
+            //extracting into a file
+            FeatureExtractor.extractFeaturesFromArrayListToFile(dataset, outputFileName, Settings.getDefaultUserId());
+            
+            //extracting into a list
+            List<Feature> featureList2 = FeatureExtractor.extractFeaturesFromArrayListToArrayListOfFeatures(dataset, Settings.getDefaultUserId());
+            //System.out.println(featureList2);
         } catch (FeatureExtractorException ex) {
             Logger.getLogger(FeatureExtractorLibraryMainTest.class.getName()).log(Level.SEVERE, null, ex);
         }
-        try {
-            System.out.println(FeatureExtractor.extractFeaturesFromArrayListToArrayListOfFeatures(dataset, "1"));
-        } catch (FeatureExtractorException ex) {
-            Logger.getLogger(FeatureExtractorLibraryMainTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("done");*/
-        //mergeArffFilesEqually("features_Dummy.arff", "features_L.arff");
     }
-    
 }
